c27cba67ded98fe8f54a2a88965b89133ffb6e38
raotalha997az <talharao997az@gmail.com>
2025-02-27 03:29:47 +0500

    Update index.blade.php

--- resources/views/nursing_form/index.blade.php
+++ resources/views/nursing_form/index.blade.php
@@ -2,13 +2,13 @@
 @section('title')
     Nursing From List
 @endsection
-@section('content') 
+@section('content')
 <div class="container-fluid">
     <div class="d-flex flex-column">
         @include('flash::message')
         @role('Admin|Nurse|Receptionist')
         <div class="col-md-12 mb-5 text-end">
-            <a href="{{ route('nursing-form.create') }}" target="_blank"><button class="btn btn-primary">Add New Nursing Form</button></a>
+            <a href="{{ route('nursing-form.create') }}" target="_blank"><button class="cutomsmsButton">Add New Nursing Form</button></a>
         </div>
         @endrole
         <div class="table-responsive">
