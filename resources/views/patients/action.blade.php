f55d00645e5aa9bd7044dad286f166e1761afd12
AR searchglobal <abdulraheem@searchglobal.org>
2025-02-27 05:47:23 +0500

    Update action.blade.php

--- resources/views/patients/action.blade.php
+++ resources/views/patients/action.blade.php
@@ -7,6 +7,7 @@
 
 @role('Admin|CSR')
     @role('Admin')
+
     <a href="{{ route('patients.edit',$row->id)}}" title="{{__('messages.common.edit') }}"
        class="btn px-1 text-primary fs-3 ps-0">
         <i class="fa-solid fa-pen-to-square"></i>
@@ -16,16 +17,17 @@
         <i class="fa-solid fa-trash"></i>
     </a>
     @endrole
-</div>
-
-@if (auth()->user()->hasRole('CSR'))
-<a href="{{ route('patients.edit',$row->id)}}" title="{{__('messages.common.edit') }}"
-    class="btn px-1 text-primary fs-3 ps-0">
-    <i class="fa-solid fa-pen-to-square"></i>
-</a>
+    
+    @if (auth()->user()->hasRole('CSR'))
+    <a href="{{ route('patients.edit',$row->id)}}" title="{{__('messages.common.edit') }}"
+        class="btn px-1 text-primary fs-3 ps-0">
+        <i class="fa-solid fa-pen-to-square"></i>
+    </a>
 <a href="javascript:void(0)" title="{{__('messages.common.delete')}}" data-id="{{ $row->id }}"
     class="delete-patient-btn btn px-1 text-danger fs-3 pe-0" wire:key="{{$row->id}}">
     <i class="fa-solid fa-trash"></i>
 </a>
 @endif
 @endrole
+
+</div>
